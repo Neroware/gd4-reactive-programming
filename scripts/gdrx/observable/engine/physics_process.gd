bca36dba64cdd31f7d8224dc0a943b00d132e597
Raoul Zebisch <neroware@freenet.de>
2022-07-01 20:49:22 +0200

    Added PhysicsProcess as Observable

---
+++ scripts/gdrx/observable/engine/physics_process.gd
@@ -0,0 +1,11 @@
+extends Observable
+class_name PhysicsProcessFrameObservable
+
+
+func _init(node : Node):
+	var subscribe : Callable = func(observer : ObserverBase) -> DisposableBase:
+		var tree : SceneTree = node.get_tree()
+		var sub = func(): observer.on_next(node.get_physics_process_delta_time())
+		tree.connect("physics_frame", sub)
+		return Disposable.new(func(): tree.disconnect("physics_frame", sub))
+	super._init(Subscription.new(subscribe))
--- scripts/gdrx/tester.gd
+++ scripts/gdrx/tester.gd
@@ -1,11 +1,17 @@
 extends Node
 
 var sub1 : DisposableBase
+var sub2 : DisposableBase
 
 func _ready():
-	test1()
+	#test_process()
+	test_physics()
 
 
-func test1():
+func test_process():
 	var observable = ProcessFrameObservable.new(self)
-	sub1 = observable.subscribe(func(i): print("!> ", i) ; sub1.dispose())
+	sub1 = observable.subscribe(func(i): print("dt> ", i) ; sub1.dispose())
+
+func test_physics():
+	var observable = PhysicsProcessFrameObservable.new(self)
+	sub1 = observable.subscribe(func(i): print("pdt> ", i) ; sub2.dispose())
