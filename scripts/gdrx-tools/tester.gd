6c159c2453c685cc30edcbe5292cee242d0b2972
Raoul Zebisch <neroware@freenet.de>
2022-07-04 22:11:37 +0200

    Added Timer Observables

--- scripts/gdrx-tools/tester.gd
+++ scripts/gdrx-tools/tester.gd
@@ -1,9 +1,19 @@
 extends Node
 class_name GDRxTester
 
+var sub1 : DisposableBase
+
 
 func _init():
 	pass
 
 func _ready():
-	pass
+	test_animation_player()
+
+func test_animation_player():
+	var anim : AnimationPlayer = get_node("AnimationPlayer")
+	var observable : Observable = GDRx.RxSignal.RxAnimationPlayer.AnimationFinishedAsObservable(anim)
+	sub1 = observable.subscribe(
+		func(i): print("Animation finished: " + str(i.at(0))) ; observable.dispose()
+	)
+	anim.play("foo")
--- scripts/gdrx/observable/observablebuilder.gd
+++ scripts/gdrx/observable/observablebuilder.gd
@@ -6,5 +6,5 @@ class RxSignal extends _RxSignal:
 class RxProperty:
 	pass
 
-class RxClass:
-	pass
+static func Interval(tree : SceneTree, time_sec : float, process_always : bool = true) -> TimerObservable:
+	return TimerObservable.new(tree, time_sec, process_always)
