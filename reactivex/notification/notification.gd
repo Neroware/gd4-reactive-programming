17f77717d584d4ec6124f87ba9414a6f4353231c
Raoul Zebisch <neroware@freenet.de>
2022-07-08 11:36:14 +0200

    Implemented Notifications

---
+++ reactivex/notification/notification.gd
@@ -0,0 +1,46 @@
+class_name Notification
+
+var _has_value : bool
+var _value
+var _kind : String
+
+func _init():
+	self._has_value = false
+	self._value = null
+	self._kind = ""
+
+func accept(
+	on_next, # Callable or ObserverBase
+	on_error : Callable = func(e): return,
+	on_completed : Callable = func(): return):
+		if on_next is ObserverBase:
+			return self._accept_observer(on_next)
+		
+		return self._accept(on_next, on_error, on_completed)
+
+func _accept(
+	on_next : Callable,
+	on_error : Callable = func(e): return,
+	on_completed : Callable = func(): return):
+		push_error("No implementation here!")
+
+func _accept_observer(observer : ObserverBase):
+	push_error("No implementation here!")
+
+func to_observable(): # -> Observable:
+	# TODO
+	return null
+
+func equals(other : Notification) -> bool:
+	var other_string = "" if other == null else str(other)
+	return str(self) == other_string
+
+static func from_notifier(handler : Callable) -> Observer:
+	var _on_next = func(value):
+		return handler.call(load("res://reactivex/notification/onnext.gd").new(value))
+	var _on_error = func(err):
+		return handler.call(load("res://reactivex/notification/onerror.gd").new(err))
+	var _on_completed = func():
+		return handler.call(load("res://reactivex/notification/oncompleted.gd").new())
+	
+	return Observer.new(_on_next, _on_error, _on_completed)
